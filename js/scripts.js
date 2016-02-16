cfe2df88d0700b53c8e193b88e1135fae5b36514
Genevieve Latimer and Priya Vasudevan <genevievelatimer@gmail.com>
2016-02-15 20:26:27 -0800

    initialize new project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,1 @@
-$(document).ready(function() {
-  var flavors = ["Chocolate, ", "Strawberry, ", "Vanilla, ", "Pistachio, ", "Black Currant."];
-  flavors.forEach(function(flavor) {
-    $("ul#flavors").append(flavor);
-  })
-});
+
