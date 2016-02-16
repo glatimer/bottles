6e2b3567e7a89bfeda55cd894c1134dcd78cb855
Genevieve Latimer and Priya Vasudevan <satipriya99@gmail.com>
2016-02-15 21:07:00 -0800

    Fix js to finish song.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-
+$(document).ready(function() {
+  for (var index = 99; index >= 0; index -= 1) {
+    alert(index + " bottles of beer on the wall. Take one down, pass it around " + (index-1) + " bottles of beer on the wall.");
+  }
+});
